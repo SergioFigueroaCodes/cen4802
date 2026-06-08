81791723318d981756d7fa776f911a7435785156
sergiofigueroa <sergiofigueroa19@yahoo.com>
2026-06-08 15:40:32 -0400

    Initial Fibonacci program

---
+++ src/FibonacciCalculator.java
@@ -0,0 +1,20 @@
+public class FibonacciCalculator {
+
+    public static int fibonacci(int n) {
+
+        if (n <= 1) {
+            return n;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+
+        int term = 10;
+        int result = fibonacci(term);
+
+        System.out.println("The " + term +
+                "th term of the Fibonacci sequence is " + result + ".");
+    }
+}
